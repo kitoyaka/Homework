1da4dd502a86b9838eca766e504e684640ea8d18
kitoyaka <vlasiukdima007@gmail.com>
2023-10-28 09:16:55 +0300

    Second commit test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.out.println("Hello world number 2");
         prif obj1 = new prif();
         obj1.method();
-        System.out.println("obj1 =" + obj1);
+        int l = obj1.x;
+        System.out.println("obj1 =" + l);
     }
 }
