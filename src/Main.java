aee3a9928ecc70720e88481ce8fce2ceba4373f7
kitoyaka <vlasiukdima007@gmail.com>
2023-10-29 10:20:16 +0200

    Third commit test

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world number 2");
-        prif obj1 = new prif();
-        obj1.method();
-        System.out.println("obj1 =" + obj1);
-        thirdClass obj2 = new thirdClass();
-        obj2.method2();
     }
 }
--- src/thirdClass.java
+++
@@ -1,7 +0,0 @@
-public class thirdClass {
-
-    public void method2() {
-        System.out.println("1");
-        System.out.println("2");
-    }
-}
