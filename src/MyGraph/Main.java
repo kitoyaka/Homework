3ad2ddd241c356ed2094da7c7dceaa5dd92f55b0
kitoyaka <vlasiukdima007@gmail.com>
2023-10-29 11:05:00 +0200

    Third commit test

---
+++ src/MyGraph/Main.java
@@ -0,0 +1,46 @@
+package MyGraph;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ScribbleTest extends Frame{
+    private static final long serialVersionUID = 1L;
+    public ScribbleTest (String s){
+        super(s);
+        ScrollPane pane = new ScrollPane();
+        pane.setSize(300,300);
+        add(pane, BorderLayout.CENTER);
+        Scribble scr = new Scribble(this, 500, 500);
+        pane.add(scr);
+        Panel p = new Panel();
+        add(p, BorderLayout.SOUTH);
+        Button b1= new Button("Red");
+        p.add(b1);
+        b1.addActionListener(scr);
+        Button b2 = new Button("Green");
+        p.add(b2);
+        b2.addActionListener(scr);
+        Button b3= new Button("Blue");
+        p.add(b3);
+        b3.addActionListener(scr);
+        Button b4= new Button("Black");
+        p.add(b4);
+        b4.addActionListener(scr);
+        Button b5= new Button("Clean");
+        p.add(b5);
+        b5.addActionListener(scr);
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing (WindowEvent e){
+                System.exit(0);
+            }
+        });
+        pack();
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new ScribbleTest(" \"Painting\"");
+    }
+}
+class Scribble extends Component implements  ActionListener, MouseListener, MouseMotionListener{
+
+}
--- src/prif.java
+++
@@ -1,9 +0,0 @@
-public class prif {
-    int x, y;
-    prif(){
-        x = 5; y = 3;
-    }
-    public void method(){
-        int z = x + y;
-    }
-}
