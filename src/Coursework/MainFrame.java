a0fae5c5fd5f45c830cd296a21b55cb4db51096c
kitoyaka <vlasiukdima007@gmail.com>
2023-11-04 15:25:53 +0200

    Добавив друге фото

--- src/Coursework/MainFrame.java
+++ src/Coursework/MainFrame.java
@@ -17,6 +17,7 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     Image firstStokPicture;
+    Image firstLeftLeverRed;
     public void firstPhoto(){
         String firstStokPicturePath = "photos/firstStokPhoto.png";
         try {
@@ -25,13 +26,40 @@ public class MainFrame extends JFrame {
         // Оброблення помилки. Якщо фото не буде знайдене по його PATH, вийде помилка IOException.
         // Тому в дужках вказується ТИП помилки і виводиться щось якщо вона станеться.
         catch (IOException e){
+            e.printStackTrace();
             getGraphics().drawString("Зображення не знайдено", 110,120);
         }
-        getGraphics().drawImage(firstStokPicture, 5,25,this);
+    }
+    public void leftLever(){
+        JButton lever1 = new JButton(" ");
+        lever1.setBounds(370,140, 35, 75);
+        lever1.setVisible(true);
+        lever1.setEnabled(true);
+        add(lever1);
+        lever1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstLeftLeverRedPath = "photos/firstLeftLeverRed.png";
+                try {
+                    firstLeftLeverRed = ImageIO.read(new File(firstLeftLeverRedPath));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    System.out.println("No photo");
+                }
+                repaint();
+            }
+        });
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawImage(firstStokPicture,5,25,this);
+        if(firstLeftLeverRed != null){
+            g.drawImage(firstLeftLeverRed,5,25,this);
+        }
     }
     public static void main(String[] args){
         MainFrame obj = new MainFrame("Курсова");
         obj.firstPhoto();
+        obj.leftLever();
     }
-
 }
