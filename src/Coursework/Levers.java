e4d69a1d56ea9ce94ade0319747f57b2f9b43a0d
kitoyaka <vlasiukdima007@gmail.com>
2023-11-11 09:42:29 +0200

    Курсова зміна

---
+++ src/Coursework/Levers.java
@@ -0,0 +1,31 @@
+package Coursework;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.File;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+
+public class Levers extends MainFrame {
+    Levers(String s) {
+        super(s);
+    }
+    public void leftLever() {
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                if (x >= 370 && x <= 405 && y >= 140 && y <= 215) {
+                    try {
+                        firstLeftLeverRed = ImageIO.read(new File("photos/firstLeftLeverRed.png"));
+                        repaint();
+                    } catch (IOException ex) {
+                        System.out.println("No photo");
+                    }
+                }
+            }
+        });
+    }
+}
--- src/Coursework/MainFrame.java
+++ src/Coursework/MainFrame.java
@@ -1,65 +1,46 @@
 package Coursework;
-import  javax.imageio.ImageIO;
+import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.io.File;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.Graphics;
-public class MainFrame extends JFrame {
+import javax.swing.JFrame;
 
-    MainFrame(String s){
+
+
+public class MainFrame extends JFrame {
+    Image firstStokPicture;
+    Image firstLeftLeverRed;
+
+    MainFrame(String s) {
         super(s);
         setLayout(null);
-        setSize(1024,800);
+        setSize(1024, 800);
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-    Image firstStokPicture;
-    Image firstLeftLeverRed;
-    public void firstPhoto(){
-        String firstStokPicturePath = "photos/firstStokPhoto.png";
         try {
-            firstStokPicture = ImageIO.read(new File(firstStokPicturePath));
-        }
-        // Оброблення помилки. Якщо фото не буде знайдене по його PATH, вийде помилка IOException.
-        // Тому в дужках вказується ТИП помилки і виводиться щось якщо вона станеться.
-        catch (IOException e){
+            firstStokPicture = ImageIO.read(new File("photos/firstStokPhoto.png"));
+        } catch (IOException e) {
             e.printStackTrace();
-            getGraphics().drawString("Зображення не знайдено", 110,120);
+            System.out.println("Зображення не знайдено");
         }
     }
-    public void leftLever(){
-        JButton lever1 = new JButton(" ");
-        lever1.setBounds(370,140, 35, 75);
-        lever1.setVisible(true);
-        lever1.setEnabled(true);
-        add(lever1);
-        lever1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String firstLeftLeverRedPath = "photos/firstLeftLeverRed.png";
-                try {
-                    firstLeftLeverRed = ImageIO.read(new File(firstLeftLeverRedPath));
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    System.out.println("No photo");
-                }
-                repaint();
-            }
-        });
-    }
-    public void paint(Graphics g){
+    @Override
+    public void paint(Graphics g) {
         super.paint(g);
-        g.drawImage(firstStokPicture,5,25,this);
-        if(firstLeftLeverRed != null){
-            g.drawImage(firstLeftLeverRed,5,25,this);
+        g.drawImage(firstStokPicture, 5, 25, this);
+        if (firstLeftLeverRed != null) {
+            g.drawImage(firstLeftLeverRed, 5, 25, this);
         }
     }
-    public static void main(String[] args){
-        MainFrame obj = new MainFrame("Курсова");
-        obj.firstPhoto();
-        obj.leftLever();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame obj = new MainFrame("Курсова");
+            Levers levers = new Levers(" ");
+            levers.leftLever();
+        });
     }
 }
