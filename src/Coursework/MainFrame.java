0064ee936a9db20fda70e3d4d83183ff35d4ba5c
kitoyaka <vlasiukdima007@gmail.com>
2023-11-11 10:26:36 +0200

    Курсова зміна

--- src/Coursework/MainFrame.java
+++ src/Coursework/MainFrame.java
@@ -1,17 +1,17 @@
 package Coursework;
+
 import javax.imageio.ImageIO;
-import java.io.IOException;
-import java.io.File;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.*;
-import javax.swing.JFrame;
-
-
 
 public class MainFrame extends JFrame {
-    Image firstStokPicture;
-    Image firstLeftLeverRed;
+     Image firstStokPicture;
+     Image firstLeftLeverRed;
+     Timer timer;
+     boolean switchImage = false;
 
     MainFrame(String s) {
         super(s);
@@ -20,12 +20,37 @@ public class MainFrame extends JFrame {
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         try {
-            firstStokPicture = ImageIO.read(new File("photos/firstStokPhoto.png"));
+            firstLeftLeverRed = ImageIO.read(new File("photos/firstStokPhoto.png"));
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Зображення не знайдено");
         }
+        timer = new Timer(2000, new ActionListener() { //2000 = 2 sec
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchImage = !switchImage; // Переключаем значение для смены изображения
+                if (switchImage) {
+                    try {
+                        firstLeftLeverRed = ImageIO.read(new File("photos/firstLeftLeverRedOff.png"));
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        System.out.println("No photo");
+                    }
+                } else {
+                    firstLeftLeverRed = null; //
+                }
+                repaint(); //
+                timer.stop();
+            }
+        });
+
+        addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e){
+                timer.start();
+            }
+        });
     }
     @Override
     public void paint(Graphics g) {
@@ -35,7 +60,6 @@ public class MainFrame extends JFrame {
             g.drawImage(firstLeftLeverRed, 5, 25, this);
         }
     }
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             MainFrame obj = new MainFrame("Курсова");
