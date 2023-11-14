1893ee4e79f10c46720a62654453b13354f94139
kitoyaka <vlasiukdima007@gmail.com>
2023-11-14 23:40:43 +0200

    Курсова зміна

--- src/Coursework/Levers.java
+++ src/Coursework/Levers.java
@@ -22,25 +22,78 @@ public class Levers extends MainFrame {
                         firstLeftLeverRed = ImageIO.read(new File("photos/firstLeftLeverRed.png"));
                         repaint();
                     } catch (IOException ex) {
-                        System.out.println("No photo");
+                        System.out.println("NO PHOTO");
                     }
+                    timer = new Timer(2000, new ActionListener() { // 2000 = 2 sec
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            switchImage = !switchImage;
+                            if (switchImage) {
+                                try {
+                                    firstLeftLeverRed = ImageIO.read(new File("photos/firstLeftLeverGreen.jpeg"));
+                                    repaint();
+                                } catch (IOException ex) {
+                                    ex.printStackTrace();
+                                    System.out.println("No photo");
+                                }
+                            } else {
+                                firstLeftLeverRed = null; //
+                            }
+                            timer.stop();
+                        }
+                    });
+                    addMouseListener(new MouseAdapter() {
+                        public void mouseClicked(MouseEvent e){
+                            timer.start();
+                        }
+                    });
                 }
             }
         });
+
     }
-    public void rightLever(){
+    //////
+    public void rightLever() {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 int x = e.getX();
                 int y = e.getY();
-                if(x >= 645 && x <= 685 && y >= 140 && y <=215){
-                   try{
-                       firstRightLeverRed = ImageIO.read(new File("photos/"))
-                   }
+                if (x >= 645 && x <= 685 && y >= 140 && y <=215) {
+                    try {
+                        firstRightLeverRed = ImageIO.read(new File("photos/firstRightLeverRed.png"));
+                        repaint();
+                    } catch (IOException ex) {
+                        System.out.println("NO PHOTO");
+                    }
+                    timer = new Timer(2000, new ActionListener() { // 2000 = 2 sec
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            switchImage = !switchImage;
+                            if (switchImage) {
+                                try {
+                                    firstRightLeverRed = ImageIO.read(new File("photos/firstRightLeverGreen.jpeg"));
+                                    repaint();
+                                } catch (IOException ex) {
+                                    ex.printStackTrace();
+                                    System.out.println("No photo");
+                                }
+                            } else {
+                                firstRightLeverRed = null; //
+                            }
+                            timer.stop();
+                        }
+                    });
+                        addMouseListener(new MouseAdapter() {
+                            public void mouseClicked(MouseEvent e){
+                                timer.start();
+                            }
+                        });
                 }
+
             }
         });
+
     }
 
 }
--- src/Coursework/MainFrame.java
+++ src/Coursework/MainFrame.java
@@ -21,44 +21,24 @@ public class MainFrame extends JFrame {
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
         try {
-            firstLeftLeverRed = ImageIO.read(new File("photos/firstStokPhoto.png"));
+            firstStokPicture = ImageIO.read(new File("photos/firstStokPhoto.png"));
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Зображення не знайдено");
         }
-        timer = new Timer(2000, new ActionListener() { // 2000 = 2 sec
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switchImage = !switchImage;
-                if (switchImage) {
-                    try {
-                        firstLeftLeverRed = ImageIO.read(new File("photos/firstLeftLeverGreen.jpeg")); // путь до фотки яка нам треба
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                        System.out.println("No photo");
-                    }
-                } else {
-                    firstLeftLeverRed = null; //
-                }
-                repaint(); //
-                timer.stop();
-            }
-        });
 
-        addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e){
-                timer.start();
-            }
-        });
     }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         g.drawImage(firstStokPicture, 5, 25, this);
         if (firstLeftLeverRed != null) {
             g.drawImage(firstLeftLeverRed, 5, 25, this);
+        }
+        if(firstRightLeverRed != null){
+            g.drawImage(firstRightLeverRed,5,25,this);
         }
     }
     public static void main(String[] args) {
