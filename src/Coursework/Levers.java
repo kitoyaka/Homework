4e1229daccd5b30e40824d19ac8b81c42859fef8
kitoyaka <vlasiukdima007@gmail.com>
2023-11-15 00:04:38 +0200

    Курсова зміна

--- src/Coursework/Levers.java
+++ src/Coursework/Levers.java
@@ -24,76 +24,68 @@ public class Levers extends MainFrame {
                     } catch (IOException ex) {
                         System.out.println("NO PHOTO");
                     }
-                    timer = new Timer(2000, new ActionListener() { // 2000 = 2 sec
+
+                     timer = new Timer(2000, new ActionListener() { // 2000 = 2 сек
                         @Override
                         public void actionPerformed(ActionEvent e) {
                             switchImage = !switchImage;
                             if (switchImage) {
                                 try {
                                     firstLeftLeverRed = ImageIO.read(new File("photos/firstLeftLeverGreen.jpeg"));
-                                    repaint();
+                                    timer.stop();
                                 } catch (IOException ex) {
                                     ex.printStackTrace();
-                                    System.out.println("No photo");
+                                    System.out.println("Немає фото");
                                 }
                             } else {
-                                firstLeftLeverRed = null; //
+                                firstLeftLeverRed = null;
                             }
-                            timer.stop();
+                            repaint();
                         }
                     });
-                    addMouseListener(new MouseAdapter() {
-                        public void mouseClicked(MouseEvent e){
-                            timer.start();
-                        }
-                    });
+                    timer.start();
                 }
             }
         });
-
     }
-    //////
+    ///////////////////////////////
+    ///////////////////////////////
     public void rightLever() {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 int x = e.getX();
                 int y = e.getY();
-                if (x >= 645 && x <= 685 && y >= 140 && y <=215) {
+                if (x >= 645 && x <= 685 && y >= 140 && y <= 215) {
                     try {
                         firstRightLeverRed = ImageIO.read(new File("photos/firstRightLeverRed.png"));
                         repaint();
                     } catch (IOException ex) {
                         System.out.println("NO PHOTO");
                     }
-                    timer = new Timer(2000, new ActionListener() { // 2000 = 2 sec
+                     timer = new Timer(2000, new ActionListener() { // 2000 = 2 сек
                         @Override
                         public void actionPerformed(ActionEvent e) {
                             switchImage = !switchImage;
                             if (switchImage) {
                                 try {
                                     firstRightLeverRed = ImageIO.read(new File("photos/firstRightLeverGreen.jpeg"));
-                                    repaint();
+                                    timer.stop();
                                 } catch (IOException ex) {
                                     ex.printStackTrace();
-                                    System.out.println("No photo");
+                                    System.out.println("NO PHOTO");
                                 }
                             } else {
                                 firstRightLeverRed = null; //
                             }
-                            timer.stop();
+                            repaint();
                         }
                     });
-                        addMouseListener(new MouseAdapter() {
-                            public void mouseClicked(MouseEvent e){
-                                timer.start();
-                            }
-                        });
+                    timer.start();
                 }
-
             }
         });
-
     }
+
 
 }
