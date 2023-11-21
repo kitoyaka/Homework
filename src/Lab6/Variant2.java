4d697b1f05451a27117cd58e0e4fc29a681cc9b5
kitoyaka <vlasiukdima007@gmail.com>
2023-11-21 15:39:25 +0200

    Курсова зміна

--- src/Lab6/Variant2.java
+++ src/Lab6/Variant2.java
@@ -1,37 +1,44 @@
 package Lab6;
 
-import javax.swing.*;
-import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.URL;
 
 public class Variant2 extends JFrame {
-    Variant2(){
-        super("DrawImage");
+    private BufferedImage myPicture;
+
+    Variant2() {
+        super("Variant2");
         setLayout(null);
-        setSize(355,300);
+        setSize(355, 300);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    Image myPicture;
-    public void paint(Graphics g){
+
+    public void paint(Graphics g) {
+        super.paint(g); // Очищаем окно
+
         try {
+            // Загрузка изображения в BufferedImage
             myPicture = ImageIO.read(new URL("https://i.pinimg.com/564x/d7/12/e8/d712e82c87d68894113dd169629627e2.jpg"));
-        }
-        catch(IOException e){
+
+            // Отображение изображения
+            g.drawImage(myPicture, 50, 10, this);
+        } catch (IOException e) {
             e.printStackTrace();
+
+            // В случае ошибки отображаем сообщение
             g.setColor(Color.WHITE);
-            g.fillRect(0,0,355,300);
+            g.fillRect(0, 0, 355, 300);
             g.setColor(Color.BLACK);
-            g.drawString("Зображння не знайдено!",110,120);
+            g.drawString("Изображение не найдено!", 110, 120);
         }
-        g.drawImage(myPicture,50,10,this);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Variant2();
     }
 }
